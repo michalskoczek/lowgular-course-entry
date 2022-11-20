a93afb84edf559e9efd8555dd23ee9d41c1d0d17
Michał Skoczek <mwskoczek@gmail.com>
2022-11-20 15:59:54 +0100

    feat: add create employee method - lesson 3, ex.3

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs";
 import {PersonModel} from "../model/person.model";
+import {CreateEmployeeModel} from "../model/create-employee.model";
 
 @Injectable()
 export class EmployeeService {
@@ -9,5 +10,9 @@ export class EmployeeService {
 
   public getAll(): Observable<PersonModel[] | null> {
     return this._httpClient.get<PersonModel[] | null>('assets/data/people.json');
+  }
+
+  public create(data: CreateEmployeeModel): Observable<CreateEmployeeModel> {
+    return this._httpClient.post<CreateEmployeeModel>('https://dummy.restapiexample.com/api/v1/create', data)
   }
 }
--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import {CreateEmployeeModel} from "../../model/create-employee.model";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-employee-form',
@@ -13,4 +16,15 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null, [Validators.required, Validators.min(0)]),
   });
+
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  public onFormSubmitted(data: CreateEmployeeModel): void {
+    this._employeeService.create(data).pipe(
+      map((r: any) => r.data)
+    ).subscribe(user => {
+      alert(`User was successfully added to the database. Name: ${user.name}, age: ${user.age}, salary: ${user.salary}`)
+    })
+  }
 }
