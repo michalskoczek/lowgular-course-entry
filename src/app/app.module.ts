f2ce274b9ab6a1f1c39b19d6c545c23d303a823b
Michał Skoczek <mwskoczek@gmail.com>
2022-11-20 12:25:39 +0100

    feat: project service added - lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import { EmployeeFacesComponentModule } from "./ui/employee-faces/employee-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectServiceModule} from "./services/project.service-module";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +16,10 @@ import {EmployeeServiceModule} from "./services/employee.service-module";
     AppRoutingModule,
     HttpClientModule,
     EmployeeServiceModule,
+    ProjectServiceModule,
     EmployeeListComponentModule,
-    EmployeeFacesComponentModule
+    EmployeeFacesComponentModule,
+    ProjectListComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent],
